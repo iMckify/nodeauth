ee1c1463e7719b7fb93fb689c065d69546e905fb
rokmoc <rokas.mockevicius@gmail.com>
2019-10-20 18:11:05 +0300

    comments on CommonJS Module standard

--- backend/db.js
+++ backend/db.js
@@ -1,3 +1,7 @@
+// Node.js implements CommonJS modules standard (select this in prettier, eslint)
+// module.exports is an object which is included in current .js file in the Node.js application.
+// module in Node.js is a functionality.
+// exports is an object that will be exported as a module for using in other files.
 module.exports = {
     DB: 'mongodb://localhost:27017/auth'
 }
