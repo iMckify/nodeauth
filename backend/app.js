6e26b804c0b3fb9afb0774c1718c10166c1fcd88
rokmoc <rokas.mockevicius@gmail.com>
2019-10-20 23:08:18 +0300

    comments from documentation for clarity

--- backend/app.js
+++ backend/app.js
@@ -1,25 +1,45 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const passport = require('passport');
-const config = require('./db');
+const express = require('express'); // framework for MVC, manages Routing, handles Requests and Views
+const mongoose = require('mongoose'); // ODM(maps obj to Document DB like Mongo), connects NODE <-> MongoDB
+const bodyParser = require('body-parser'); // manages HTTP body
+const passport = require('passport'); // auth middleware
+const DBconfig = require('./db').DB; // require() = import statement in React
 
-mongoose.connect(config.DB, { useNewUrlParser: true }).then(
+const users = require('./routes/userActions'); 
+
+mongoose.connect(DBconfig, { useNewUrlParser: true }).then(
     () => {console.log('Database is connected') },
     err => { console.log('Can not connect to the database'+ err)}
 );
 
 const app = express(); // initiates express app
+// passport.initialize()- MIDDLEWARE that initialises Passport
+// Middlewares are functions that have access to the request object (req), the response
+// object (res), and the next middleware function in the application’s request-response cycle
+app.use(passport.initialize());
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+// puts passport.js config into passport middleware
+require('./passport')(passport);
 
+
+// .use() method in express is called a middleware.
+// adding middleware between brackets to app object.
+// bodyParser is executed on every request
+app.use(bodyParser.urlencoded({ extended: false })); // selects algorythm for parsing
+app.use(bodyParser.json()); // bodyParser.json returns middleware that only parses json
+
+// app.use([path,] callback [, callback...]), where 
+//  callback: (array of) middleware functions
+// mounts callback functions at the specified path
+app.use('/api/users', users);
+
+// GET requests handler
 app.get('/', function(req, res) {
     res.send('hello');
 });
 
 const PORT = process.env.PORT || 5000;
 
+//listens for connections on the specified port
 app.listen(PORT, () => {
     console.log(`Server is running on PORT ${PORT}`);
 });
