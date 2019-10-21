b2e0bca12b278f4e603969c85a1c9613b183fbc7
rokmoc <rokas.mockevicius@gmail.com>
2019-10-21 20:57:07 +0300

    took me 1 day to find, that I have put RegistrationForm instead of MainBody....

--- frontend/src/app/App.js
+++ frontend/src/app/App.js
@@ -1,5 +1,10 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import Store from '../utils/redux/store';
+import MainBody from '../containers/MainBody';
 
-export default () => <Provider store={Store} />;
+export default () => (
+  <Provider store={Store}>
+    <MainBody />
+  </Provider>
+);
