1a32651723482b31e4dfe5e5d2c38e97b62cd76c
rokmoc <rokas.mockevicius@gmail.com>
2019-10-22 12:56:10 +0300

    error index.js:10 @babel/polyfill loads more than once

--- frontend/src/containers/MainBody.js
+++ frontend/src/containers/MainBody.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import '@babel/polyfill';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
