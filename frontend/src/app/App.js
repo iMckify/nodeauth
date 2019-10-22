aa7e40ecdb456aa033781b431423f0857812c27f
rokmoc <rokas.mockevicius@gmail.com>
2019-10-22 14:22:54 +0300

    moved conditional LoginForm RegistrationForm rendering into Window (future publicWindow) function, created directories for container elements

--- frontend/src/app/App.js
+++ frontend/src/app/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import Store from '../utils/redux/store';
-import MainBody from '../containers/MainBody';
+import MainBody from '../containers/mainBody/MainBody';
 
 export default () => (
   <Provider store={Store}>
